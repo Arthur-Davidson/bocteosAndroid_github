af3005fa3f8881132587e4f910680424089bdab1
unknown <padilla.hdz.da00@gmail.com>
2025-10-01 10:45:17 -0600

    [AGREGA] controles de navegacion

--- NavigationCompose/app/src/main/java/mx/uacj/navigationcompose/ui/controladores/navegacion/navegacionInicio.kt
+++ NavigationCompose/app/src/main/java/mx/uacj/navigationcompose/ui/controladores/navegacion/navegacionInicio.kt
@@ -13,13 +13,14 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import mx.uacj.navigationcompose.ui.pantallas.PantallaVideo
 
 @Composable
 fun NavegacionInicio(modificador: Modifier = Modifier){
     val controladorNavegacion = rememberNavController()
 
-    NavHost(navController = controladorNavegacion, startDestination = "Inicio"){
-        composable("Inicio") {
+    NavHost(navController = controladorNavegacion, startDestination = Inicio){
+        composable <Inicio> {
             Column (modifier = modificador
                 .fillMaxSize()
                 .background(color = Color.Red)){
@@ -28,32 +29,25 @@ fun NavegacionInicio(modificador: Modifier = Modifier){
                 Text("Ir a Video",
                     modifier = modificador
                         .padding(15.dp)
-                        .clickable { controladorNavegacion.navigate("Videos") })
+                        .clickable { controladorNavegacion.navigate(Video) })
 
                 Text("Ir a Configuracion",
                     modifier = modificador
                         .padding(15.dp)
-                        .clickable { controladorNavegacion.navigate("Configuracion") })
+                        .clickable { controladorNavegacion.navigate(Configuracion) })
 
                 Text("Ir a Canal",
                     modifier = modificador
                         .padding(15.dp)
-                        .clickable { controladorNavegacion.navigate("Canal") })
+                        .clickable { controladorNavegacion.navigate(Canal) })
             }
         }
 
-        composable("Videos") {
-            Text("En pantalla de videos", modifier = modificador
-                .fillMaxSize()
-                .background(color = Color.Blue)
-            )
-            Text("Ir al inicio",
-                modifier = modificador
-                    .padding(15.dp)
-                    .clickable { controladorNavegacion.navigate("Inicio") })
+        composable <Video> {
+            PantallaVideo(modificador, controladorNavegacion)
         }
 
-        composable("Configuracion") {
+        composable <Configuracion> {
             Text("En pantalla de configuracion", modifier = modificador
                 .fillMaxSize()
                 .background(color = Color.Green)
@@ -61,10 +55,10 @@ fun NavegacionInicio(modificador: Modifier = Modifier){
             Text("Ir al inicio",
                 modifier = modificador
                     .padding(15.dp)
-                    .clickable { controladorNavegacion.navigate("Inicio") })
+                    .clickable { controladorNavegacion.navigate(Inicio) })
         }
 
-        composable("Canal") {
+        composable <Canal> {
             Text("En pantalla de canal", modifier = modificador
                 .fillMaxSize()
                 .background(color = Color.Yellow)
@@ -72,7 +66,7 @@ fun NavegacionInicio(modificador: Modifier = Modifier){
             Text("Ir al inicio",
                 modifier = modificador
                     .padding(15.dp)
-                    .clickable { controladorNavegacion.navigate("Inicio") })
+                    .clickable { controladorNavegacion.navigate(Inicio) })
         }
     }
 }
---
+++ NavigationCompose/app/src/main/java/mx/uacj/navigationcompose/ui/controladores/navegacion/pantallas_disponibles.kt
@@ -0,0 +1,15 @@
+package mx.uacj.navigationcompose.ui.controladores.navegacion
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Inicio
+
+@Serializable
+object Configuracion
+
+@Serializable
+object Video
+
+@Serializable
+object Canal
