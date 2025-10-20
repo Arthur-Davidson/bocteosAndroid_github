3c0d0ff7273a796815989659feca6002b6fde44f
DAVIS MSK <padilla.hdz.da00@gmail.com>
2025-10-19 21:08:34 -0600

    [AGREGA] modificacion de JSON a Kotlin de pokemon

--- PokemonAPI/app/src/main/java/mx/uacj/pokeapi/modelos/pokemon.kt
+++ PokemonAPI/app/src/main/java/mx/uacj/pokeapi/modelos/pokemon.kt
@@ -1,82 +1,114 @@
 package mx.uacj.pokeapi.modelos
 
-data class habilidadURL(
+// -------------------
+// Habilidades
+// -------------------
+data class HabilidadURL(
     val name: String,
     val url: String
 )
-data class habilidad(
+
+data class Habilidad(
     val is_hidden: Boolean,
     val slot: Int,
-    val ability: habilidadURL
+    val ability: HabilidadURL
 )
 
-{
-    {
-        "id": 1,
-        "name": "stench",
-        "is_main_series": true,
-        "generation": {
-        "name": "generation-iii",
-        "url": "https://pokeapi.co/api/v2/generation/3/"
-    },
-        "names": [
-        {
-            "name": "Stench",
-            "language": {
-            "name": "en",
-            "url": "https://pokeapi.co/api/v2/language/9/"
-        }
-        }
-        ],
-        "effect_entries": [
-        {
-            "effect": "This Pokémon's damaging moves have a 10% chance to make the target [flinch]{mechanic:flinch} with each hit if they do not already cause flinching as a secondary effect.\n\nThis ability does not stack with a held item.\n\nOverworld: The wild encounter rate is halved while this Pokémon is first in the party.",
-            "short_effect": "Has a 10% chance of making target Pokémon [flinch]{mechanic:flinch} with each hit.",
-            "language": {
-            "name": "en",
-            "url": "https://pokeapi.co/api/v2/language/9/"
-        }
-        }
-        ],
-        "effect_changes": [
-        {
-            "version_group": {
-            "name": "black-white",
-            "url": "https://pokeapi.co/api/v2/version-group/11/"
-        },
-            "effect_entries": [
-            {
-                "effect": "Has no effect in battle.",
-                "language": {
-                "name": "en",
-                "url": "https://pokeapi.co/api/v2/language/9/"
-            }
-            }
-            ]
-        }
-        ],
-        "flavor_text_entries": [
-        {
-            "flavor_text": "è‡­ãã¦ã€€ç›¸æ‰‹ãŒ\nã²ã‚‹ã‚€ã€€ã“ã¨ãŒã‚ã‚‹ã€‚",
-            "language": {
-            "name": "ja-kanji",
-            "url": "https://pokeapi.co/api/v2/language/11/"
-        },
-            "version_group": {
-            "name": "x-y",
-            "url": "https://pokeapi.co/api/v2/version-group/15/"
-        }
-        }
-        ],
-        "pokemon": [
-        {
-            "is_hidden": true,
-            "slot": 3,
-            "pokemon": {
-            "name": "gloom",
-            "url": "https://pokeapi.co/api/v2/pokemon/44/"
-        }
-        }
-        ]
-    }
-}
+// -------------------
+// Aullido (cries)
+// -------------------
+data class Aullido(
+    val latest: String?,
+    val legacy: String?
+)
+
+// -------------------
+// Sprites (imágenes)
+// -------------------
+data class Sprite(
+    val back_default: String?,
+    val back_female: String?,
+    val back_shiny: String?,
+    val back_shiny_female: String?,
+    val front_default: String?,
+    val front_female: String?,
+    val front_shiny: String?,
+    val front_shiny_female: String?
+)
+
+// -------------------
+// Stats (atributos base como speed, attack...)
+// -------------------
+data class StatURL(
+    val name: String,
+    val url: String
+)
+
+data class Stat(
+    val base_stat: Int,
+    val effort: Int,
+    val stat: StatURL
+)
+
+// -------------------
+// Tipos (fairy, fire, water...)
+// -------------------
+data class TipoURL(
+    val name: String,
+    val url: String
+)
+
+data class Tipo(
+    val slot: Int,
+    val type: TipoURL
+)
+
+// -------------------
+// Generaciones pasadas (past_types, past_abilities)
+// -------------------
+data class GenerationURL(
+    val name: String,
+    val url: String
+)
+
+data class PastType(
+    val slot: Int,
+    val type: TipoURL
+)
+
+data class PastTypeEntry(
+    val generation: GenerationURL,
+    val types: List<PastType>
+)
+
+data class PastAbility(
+    val ability: HabilidadURL?,
+    val is_hidden: Boolean,
+    val slot: Int
+)
+
+data class PastAbilityEntry(
+    val generation: GenerationURL,
+    val abilities: List<PastAbility>
+)
+
+// -------------------
+// Pokémon principal
+// -------------------
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val base_experience: Int,
+    val height: Int,
+    val is_default: Boolean,
+    val order: Int,
+    val weight: Int,
+
+    val abilities: List<Habilidad>,
+    val sprites: Sprite,
+    val cries: Aullido,
+    val stats: List<Stat>,
+    val types: List<Tipo>,
+    val past_types: List<PastTypeEntry>,
+    val past_abilities: List<PastAbilityEntry>
+)
